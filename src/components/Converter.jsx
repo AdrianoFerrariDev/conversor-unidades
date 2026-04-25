2797be2ad980d5d90ff8657893ea4d0f56142333
Adriano <adriano_ferrari_654@yahoo.com.br>
2026-04-25 14:44:30 -0300

    Refatora para componente reutilizável de conversão

---
+++ src/components/Converter.jsx
@@ -0,0 +1,77 @@
+import { useState } from "react";
+import { formatNumber } from "../utils/formatNumber";
+import { convertLength } from "../converters/length";
+
+function Converter({ units, title }) {
+    const unitKeys = Object.keys(units)
+
+    const [value, setValue] = useState(1)
+    const [from, setFrom] = useState(unitKeys[0])
+    const [to, setTo] = useState(unitKeys[1])
+
+    function convert(value, from, to) {
+        const base = value * units[from].factor
+        return base / units[to].factor
+    }
+
+    const result = formatNumber(convert(Number(value), from, to))
+
+    return (
+        <>
+            <h2>{title}</h2>
+            <input
+                type="number"
+                value={value}
+                onChange={(e) => setValue(e.target.value)}
+            />
+
+            <select value={from} onChange={(e) => setFrom(e.target.value)}>
+                {unitKeys.map((unit) => (
+                    <option key={unit} value={unit}>
+                        {units[unit].name}
+                    </option>
+                ))}
+            </select>
+            <button onClick={() => {
+                setFrom(to)
+                setTo(from)
+            }}>
+                Inverter
+            </button>
+            <select value={to} onChange={(e) => setTo(e.target.value)}>
+                {unitKeys.map((unit) => (
+                    <option key={unit} value={unit}>
+                        {units[unit].name}
+                    </option>
+                ))}
+            </select>
+            <p>Resultado {result} {units[to].symbol}</p>
+            <table>
+                <thead>
+                    <tr>
+                        <td>Valor</td>
+                        <td>Unidade</td>
+                    </tr>
+                </thead>
+                <tbody>
+                    {unitKeys.map((unit) => {
+                        const converted = convertLength(Number(value), from, unit)
+                        return(
+                            <tr
+                                key={unit}
+                                className={`table-row ${unit ===to ? 'active-row' : ''} clickable`}
+                                onClick={() => {if (unit !== to) setTo(unit)}}
+                            >
+                                <td>{formatNumber(converted)} {units[unit].symbol}</td>
+                                <td>{units[unit].name}</td>
+                            </tr>
+                        )
+                    })}
+                    
+                </tbody>
+            </table>
+        </>
+    )
+}
+
+export default Converter;
--- src/converters/length.js
+++ src/converters/length.js
@@ -1,23 +1,13 @@
 export const units = {
-    metro: {factor: 1, symbol: 'm'},
-    quilometro: {factor: 1000, symbol: 'km'},
-    decimetro: { factor: 0.1, symbol: 'dm'},
-    centimetro: {factor: 0.01, symbol: 'cm'},
-    milimetro: {factor: 0.001, symbol: 'mm'},
-    milha: {factor: 1609.34, symbol: 'mi'},
+    metro: {factor: 1, symbol: 'm', name: 'Metro'},
+    quilometro: {factor: 1000, symbol: 'km', name: 'Quilômetro'},
+    decimetro: { factor: 0.1, symbol: 'dm', name: 'Decímetro'},
+    centimetro: {factor: 0.01, symbol: 'cm', name: 'Centímetro'},
+    milimetro: {factor: 0.001, symbol: 'mm', name: 'Milímetro'},
+    milha: {factor: 1609.34, symbol: 'mi', name: 'Milha'},
 };
 
 export function convertLength(value, from, to){
     const valueInMeters = value * units[from].factor;
     return valueInMeters / units[to].factor;
-}
-
-export function formatNumber(num) {
-    if( num === 0 ) return 0;
-    
-    if( Math.abs(num) < 0.01 ) {
-        return num.toExponential(2);
-    }
-
-    return parseFloat(num.toFixed(4));
 }
