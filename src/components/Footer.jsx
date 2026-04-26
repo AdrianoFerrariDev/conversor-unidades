9eb2e5f1a151e0bb23fdf4896faec59a338a3a27
Adriano <adriano_ferrari_654@yahoo.com.br>
2026-04-26 14:28:58 -0300

    Tradução do titulo no footer para testar chave ssh

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,13 @@
+import { useTranslation } from "react-i18next";
+import i18n from '../i18n'
+
 function Footer() {
+    const { t } = useTranslation()
+
     return (
         <footer>
             <div className="container">
-                <p>© 2026 Conversor de Unidades</p>
+                <p>© 2026 {t('title')}</p>
             </div>
         </footer>
     );
