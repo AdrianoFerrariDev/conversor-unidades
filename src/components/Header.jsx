c1303ed2455582f9e79af21d70a67548f5ca273d
Adriano <adriano_ferrari_654@yahoo.com.br>
2026-05-03 20:56:22 -0300

    Css do menu retrátil para telas e alguns ajustes no html

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ import { useTranslation } from "react-i18next";
 import i18n from '../i18n'
 import { routes } from "../routes";
 import { useLanguageNavigation } from "../hooks/useLanguageNavigation";
-import { Sun, Moon } from "lucide-react";
+import { Sun, Moon, Menu, X } from "lucide-react";
 import { langList } from "../utils/langList";
 
 function Header() {
@@ -14,22 +14,23 @@ function Header() {
   : 'pt'
     
     const [theme, setTheme] = useState("light")
+    const [openMenu, setOpenMenu] = useState(false)
     const { changeLanguage } = useLanguageNavigation();
+    const saved = localStorage.getItem("theme")
 
     useEffect(() => {
         document.body.classList.remove("light", "dark")
-        document.body.classList.add = (theme);
-        document.body.className = theme //remover esta linha antes de exportar
+        document.body.classList.add(theme)
+        localStorage.setItem("theme", theme)
     }, [theme]);
 
     useEffect(() => {
-        const saved = localStorage.getItem("theme")
         if (saved) setTheme(saved)
     }, [])
 
     useEffect(() => {
-        localStorage.setItem("theme", theme)
-    }, [theme])
+        document.querySelector('.nav-menu').classList.toggle('open-menu')
+    },[openMenu])
 
     return (
         <header>
@@ -40,6 +41,20 @@ function Header() {
                 </div>
                     <div className="menu">
                         <nav className="nav-menu">
+                            <div className="menu-buttons">
+                                <button
+                                    className="theme-mode"
+                                    onClick={() => setTheme(theme === 'light' ? "dark" : "light")}
+                                >
+                                    {theme === "light" ? <Moon size={22} /> : <Sun size={22} />}
+                                </button>
+                                <button
+                                    className="menu-button"
+                                    onClick={() => setOpenMenu(openMenu === true ? false : true)}
+                                >
+                                    {openMenu === false ? <Menu size={22} /> : <X size={22} />}
+                                </button>
+                            </div>
                             <ul>
                                 <li><Link to={`/${lang}`}>Home</Link></li>
                                 <li><Link to={`/${lang}/${routes[lang].length}`}>{t('length')}</Link></li>
@@ -50,25 +65,19 @@ function Header() {
                             </ul>
                         </nav>
                         <div className="change-lang">
-                            <select className="select-lang" onChange={(e) => changeLanguage(e.target.value)}>
+                            <select name="site-lang" className="select-lang" onChange={(e) => changeLanguage(e.target.value)}>
                                 {Object.entries(langList).map(([keyLang, langName]) => (
                                 <option key={keyLang} value={keyLang}>{langName}</option>   
                                 ))}
                             </select>
                         </div>
                     </div>
+                    {/* 
                     <div className="lang-buttons">
                         <button onClick={() => changeLanguage('pt')}>PT</button>
                         <button onClick={() => changeLanguage('en')}>EN</button>
                     </div>
-                    <div className="mode-buttons">
-                        <button
-                            className="theme-toggle"
-                            onClick={() => setTheme(theme === 'light' ? "dark" : "light")}
-                        >
-                            {theme === "light" ? <Moon size={22} /> : <Sun size={22} />}
-                        </button>
-                    </div>
+                    */}
             </div>
         </header>
     );
